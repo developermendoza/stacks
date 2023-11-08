61a291a291ec74ec0af522256b576e65b206c96c
Jose Mendoza <developermendoza@gmail.com>
2023-11-08 06:42:00 -0800

    add stacks

---
+++ main.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const stack1 = new Stack();
+stack1.push("value1");
+stack1.push("value2");
+stack1.push("value3");
+
+console.log(stack1.first);
+console.log(stack1.last);
+console.log(stack1.size);
+
+stack1.push("value4");
+console.log(stack1.pop());
